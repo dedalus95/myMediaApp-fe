e139d176fd9b4cbe37e9e857873c3e3c657c4e3c
Emanuele Cerilli <ecerilli1@gmail.com>
2022-02-27 18:14:24 +0100

    create modalService

---
+++ src/app/components/dialogue/dialogue.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogueComponent } from './dialogue/dialogue.component';
+
+
+
+@NgModule({
+  declarations: [
+    DialogueComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DialogueModule { }
