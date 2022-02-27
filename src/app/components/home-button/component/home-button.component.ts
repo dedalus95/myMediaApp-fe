703ae9938f22341f849ad1c9f5c2d8b0e99e069c
Emanuele Cerilli <ecerilli1@gmail.com>
2022-02-27 12:37:44 +0100

    add router navigation to atomized home-buttons

--- src/app/components/home-button/component/home-button.component.ts
+++ src/app/components/home-button/component/home-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'mma-home-button',
